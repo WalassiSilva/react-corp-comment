0f463e443cfd015509f29efad93b7643d1fa062f
Walassi Silva <walassi.b@gmail.com>
2024-10-30 22:15:27 -0300

    feat: add basic validation to `FeedbackForm`

--- src/components/header/FeedbackForm.tsx
+++ src/components/header/FeedbackForm.tsx
@@ -1,13 +1,29 @@
+import { useState } from "react";
+import { MAX_CHARACTERS } from "../../constants";
 
 export default function FeedbackForm() {
+  const [text, setText] = useState("");
+  const charCount = MAX_CHARACTERS - text.length;
+
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const newText = e.target.value;
+    if (newText.length > MAX_CHARACTERS) return;
+    setText(newText);
+  };
   return (
     <form className="form">
-      <textarea id="feedback-textarea" placeholder="Write your feedback" spellCheck={false} />
+      <textarea
+        id="feedback-textarea"
+        placeholder="Write your feedback"
+        spellCheck={false}
+        value={text}
+        onChange={handleChange}
+      />
       <label htmlFor="feedback-textarea">
         Write your feedback here, remember to #hashtag the company
       </label>
       <div>
-        <p className="u-italic">150</p>
+        <p className="u-italic">{charCount}</p>
         <button>
           <span>submit</span>
         </button>
