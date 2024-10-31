c5df43846230b53d363baadb6e81d5a3a2c2dbfd
Walassi Silva <walassi.b@gmail.com>
2024-10-31 07:00:39 -0300

    feat: fetch data from api

--- src/components/FeedbackItem.tsx
+++ src/components/FeedbackItem.tsx
@@ -1,20 +1,30 @@
 import { TriangleUpIcon } from "@radix-ui/react-icons";
-
-export default function FeedbackItem({ feedbackItem }) {
+type FeedbackItemProps = {
+  feedbackItem: FeedbackItem;
+};
+type FeedbackItem = {
+  id: 1;
+  company: string;
+  badgeLetter: string;
+  upvoteCount: number;
+  daysAgo: number;
+  text: number;
+};
+export default function FeedbackItem({ feedbackItem }: FeedbackItemProps) {
   return (
     <li className="feedback">
       <button>
         <TriangleUpIcon />
-        <span>{feedbackItem.upvotes}</span>
+        <span>{feedbackItem.upvoteCount}</span>
       </button>
 
       <div>
-        <p>{feedbackItem.badge}</p>
+        <p>{feedbackItem.badgeLetter}</p>
       </div>
 
       <div>
-        <p>{feedbackItem.name}</p>
-        <p>{feedbackItem.feedback}</p>
+        <p>{feedbackItem.company}</p>
+        <p>{feedbackItem.text}</p>
       </div>
       <p>{feedbackItem.daysAgo}d</p>
     </li>
